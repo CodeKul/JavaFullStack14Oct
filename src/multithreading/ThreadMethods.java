61b25055d630aec30c51c31940c8ff62223b8fdb
codekulvaibhav <codekul.vaibhav@gmail.com>
2021-12-03 13:15:37 +0530

    Thread Methods and ThreadPriority

---
+++ src/multithreading/ThreadMethods.java
@@ -0,0 +1,20 @@
+package multithreading;
+
+public class ThreadMethods {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+                System.out.println(Thread.currentThread().getId());
+                System.out.println(Thread.currentThread().getPriority());
+            }
+        });
+
+        t1.setName("MyThread");
+
+        t1.start();
+    }
+}
---
+++ src/multithreading/ThreadPriority.java
@@ -0,0 +1,26 @@
+package multithreading;
+
+public class ThreadPriority {
+
+    public static void main(String[] args) {
+        Thread t1  = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("T1 thread");
+            }
+        });
+
+        Thread t2  = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("T2 thread");
+            }
+        });
+
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY);
+
+        t1.start();
+        t2.start();
+    }
+}
