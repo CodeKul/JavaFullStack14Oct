2fd1f1e889ff1673734e59826722b29297783007
codekulvaibhav <codekul.vaibhav@gmail.com>
2021-12-06 13:26:24 +0530

    Inter Thread Communication

--- src/multithreading/ThreadMethods.java
+++ src/multithreading/ThreadMethods.java
@@ -7,14 +7,17 @@ public class ThreadMethods {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println(Thread.currentThread().getName());
-                System.out.println(Thread.currentThread().getId());
-                System.out.println(Thread.currentThread().getPriority());
+//                System.out.println(Thread.currentThread().getName());
+//                System.out.println(Thread.currentThread().getId());
+//                System.out.println(Thread.currentThread().getPriority());
+//                System.out.println(Thread.currentThread().isAlive());
+                System.out.println(Thread.currentThread().isDaemon());
+
             }
         });
 
         t1.setName("MyThread");
-
+        t1.setDaemon(true);
         t1.start();
     }
 }
